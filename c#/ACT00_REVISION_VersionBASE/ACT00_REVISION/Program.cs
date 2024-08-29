f12662970f2dc14b2822c3acb83f987eca296474
Limacha <77573068+Limacha@users.noreply.github.com>
2024-08-29 13:39:42 +0200

    revi

---
+++ c#/ACT00_REVISION_VersionBASE/ACT00_REVISION/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ACT00_REVISION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // déclaration des variables.... COMPLETER AVEC CE QUI MANQUE
+
+            string rep;
+            MethodesDuProjet func = new MethodesDuProjet();
+            double c1 = 0;
+            double c2 = 0;
+            double c3 = 0;
+            bool ok = false;
+
+            Console.WriteLine("Testez les polygones !");
+            //On recommence tant que désiré
+            do
+            {
+                //lecture des 3 côtés
+                c1 = lireDouble(1);
+                c2 = lireDouble(2);
+                c3 = lireDouble(3);
+
+                // ordonner les côtés => APPEL ORDONNECOTES
+                func.OrdonneCotes(ref c1, ref c2, ref c3);
+                // série de test (voir consignes)
+                if (func.Triangle(c1, c2, c3))
+                {
+                    // préparation et affichage du résultat du test 'triangle' avec la procédure 'Affiche'
+                    func.Affiche(out rep, "triangle", true);
+
+                    // vérification équilatéral
+                    if (func.Equi(c1, c2, c3))
+                    {
+                        // préparation et affichage du résultat du test 'equilateral' avec la procédure 'Affiche'
+                        func.Affiche(out rep, "equilateral", true);
+                    }
+                    else
+                    {
+                        // vérification triangle rectangle
+                        if (func.Equi(c1, c2, c3))
+                        {
+                            // préparation et affichage du résultat positif du test 'rectangle' avec la procédure 'Affiche'
+                            func.Affiche(out rep, "rectangle", true);
+                        }
+                        else
+                        {
+                            // préparation et affichage du résultat négatif du test 'rectangle' avec la procédure 'Affiche'
+                            func.Affiche(out rep, "rectangle", false);
+                        }
+                        // vérification du cas isocèle et affichage dans le cas positif
+                        func.Isocele(c1, c2, c3, out ok);
+                        func.Affiche(out rep, "isocele", ok);
+                        //... A vous de voir en combien de lignes...
+                    }
+                }
+                else // si ce n'est pas un triangle
+                {
+                    // préparation et affichage du résultat négataif du test 'triangle' avec la procédure 'Affiche'
+                    func.Affiche(out rep, "triangle", false);
+                }
+                // reprise ?
+                Console.WriteLine("Voulez-vous tester un autre polygône ? (Tapez espace)");
+                rep = Console.ReadLine();
+            } while (rep == " ");
+        }
+        //Récupération d'une donnée fournie par l'utilisateur en 'double' : on suppose qu'il ne se trompe pas !
+        static double lireDouble(int numeroCote)
+        {
+            double cote;
+            Console.Write("Tapez la valeur du côté " + numeroCote + " : ");
+            cote = double.Parse(Console.ReadLine());
+            return cote;
+        }
+    }
+}

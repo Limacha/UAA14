7d83a1710d169bd3da2245f8bb4a4254b06efb2d
Limacha <77573068+Limacha@users.noreply.github.com>
2024-08-29 14:16:53 +0200

    ex2

---
+++ c#/6TTI_NicolasPonchaut_ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _6TTI_NicolasPonchaut_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+                func methods = new func();
+
+                methods.colorConsole(ConsoleColor.Black, ConsoleColor.Green);
+
+
+                double[] table = new double[10];
+                double x;
+
+                Console.WriteLine("Entrez 10 nombres afin d'en ressortir la moyenne:");
+                for (int i = 0; i < table.Length; i++)
+                {
+                    methods.LireDouble(i , out x);
+                    table[i] = x;
+
+                }
+                double addition = 0;
+                for (int i = 0; i < table.Length; i++)
+                {
+                    addition += table[i];
+                }
+                double final = addition / table.Length;
+                Console.WriteLine("\nMoyenne de tous ces nombres: " + final);
+
+            
+        }
+    }
+}
---
+++ c#/6TTI_NicolasPonchaut_ex2/fonction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TTI_NicolasPonchaut_ex2
+{
+    public struct func
+    {
+        public void LireDouble(int i, out double x)
+        {
+            string n;
+            do
+            {
+                Console.WriteLine("double a lire" + i);
+                n = Console.ReadLine();
+            } while (!double.TryParse(n, out x));
+        }
+
+        public void colorConsole(ConsoleColor bg, ConsoleColor text)
+        {
+            Console.BackgroundColor = bg;
+            Console.ForegroundColor = text;
+
+        }
+    }
+}

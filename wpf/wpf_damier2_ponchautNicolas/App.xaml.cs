4cd48f7ae6ac4eef5989a0e401b2ac92188f8b52
Limacha <77573068+Limacha@users.noreply.github.com>
2024-11-28 10:52:30 +0100

    damier 1-2

---
+++ wpf/wpf_damier2_ponchautNicolas/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace wpf_damier2_ponchautNicolas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ wpf/wpf_damier2_ponchautNicolas/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_damier2_ponchautNicolas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+        public void Init()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+                ColumnDefinition columnDefinition = new ColumnDefinition();
+                RowDefinition rowDefinition = new RowDefinition();
+
+                body.ColumnDefinitions.Add(columnDefinition);
+                body.RowDefinitions.Add(rowDefinition);
+            }
+
+            int nCase = 1;
+            bool white = false;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Button newCase = new Button();
+                    newCase.Content = nCase++;
+                    newCase.Foreground = Brushes.Red;
+                    newCase.FontWeight = FontWeights.Bold;
+                    newCase.Click += HideText;
+
+                    newCase.Width = 65;
+                    newCase.Height = 65;
+
+                    if (white)
+                    {
+                        newCase.Background = Brushes.White;
+                    }
+                    else
+                    {
+                        newCase.Background = Brushes.Black;
+                    }
+                    white = !white;
+
+                    Grid.SetColumn(newCase, j);
+                    Grid.SetRow(newCase, i);
+                    body.Children.Add(newCase);
+                }
+                i++;
+                for (int j = 9; j >= 0; j--)
+                {
+                    Button newCase = new Button();
+                    newCase.Content = nCase++;
+                    newCase.Foreground = Brushes.Red;
+                    newCase.FontWeight = FontWeights.Bold;
+                    newCase.Click += HideText;
+
+                    newCase.Width = 65;
+                    newCase.Height = 65;
+
+                    if (white)
+                    {
+                        newCase.Background = Brushes.White;
+                    }
+                    else
+                    {
+                        newCase.Background = Brushes.Black;
+                    }
+                    white = !white;
+
+                    Grid.SetColumn(newCase, j);
+                    Grid.SetRow(newCase, i);
+                    body.Children.Add(newCase);
+                }
+            }
+        }
+
+        private void HideText(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Content = string.Empty;
+        }
+    }
+}
